210195c03231eab055d399476d23a55d41c94329
keerthi0201 <keerthikadeva08@gmail.com>
2026-01-10 13:27:28 +0530

    Add files via upload

---
+++ arithmetic operator.java
@@ -0,0 +1,20 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=10;
+	    int b= 20;
+	    int c=a + b;
+	    int d=a- b;
+	    int e=a*b;
+	    int f=a/b;
+	    
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		
+		
+		
+		
+	}
+}
---
+++ greatest amg 3.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        if ((a > b) && (a > c)) {
+            System.out.println("a is greater");
+        } else if ((b > c) && (b > a)) {
+            System.out.println("b is greater");
+        } else {
+            System.out.println("c is greater");
+        }
+    }
+}
