574b436b3751dc599ada13c5f3abe80cec010071
keerthi0201 <keerthikadeva08@gmail.com>
2026-02-11 18:48:33 +0530

    Add files via upload

---
+++ java practice programs/charactercount.java
@@ -0,0 +1,22 @@
+package pst;
+
+import java.util.Scanner;
+
+public class charactercount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str = sc.nextLine();
+        System.out.print("Enter the character to count: ");
+        char ch = sc.next().charAt(0);
+        int count = 0;
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == ch) {
+                count++;
+            }
+        }
+        System.out.println("Count = " + count);
+
+        sc.close();
+    }
+}
