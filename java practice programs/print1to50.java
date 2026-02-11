0e0382bee9d79aa8ed17e53cea6eba459a51d803
keerthi0201 <keerthikadeva08@gmail.com>
2026-02-11 18:05:08 +0530

    Add files via upload

---
+++ java practice programs/print1to50.java
@@ -0,0 +1,14 @@
+package pst;
+
+class Print1To50 {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 50) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
---
+++ java practice programs/sumandaverage.java
@@ -0,0 +1,30 @@
+package pst;
+import java.util.Scanner;
+
+class SumAndAverage {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter how many numbers: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for(int i = 1; i <= n; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int num = sc.nextInt();
+            sum = sum + num;
+        }
+
+        double average = (double) sum / n;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+
+        sc.close();
+    } 
+
+	}
+
+
