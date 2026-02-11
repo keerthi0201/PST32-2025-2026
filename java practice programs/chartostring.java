1ea63bdb48a83d3e8461c065a13b19a50eef8a97
keerthi0201 <keerthikadeva08@gmail.com>
2026-02-11 19:13:00 +0530

    Add files via upload

---
+++ java practice programs/chartostring.java
@@ -0,0 +1,13 @@
+package pst;
+
+public class chartostring {
+
+    public static void main(String[] args) {
+
+        char ch = 'k';
+
+        String s = ch + "";
+
+        System.out.println(s);
+    }
+}
---
+++ java practice programs/stringtochar.java
@@ -0,0 +1,12 @@
+package pst;
+
+public class stringtochar {
+
+	public static void main(String[] args) {
+		String s = "keerthi";
+		for(int i = 0; i< s.length();i++) {
+			char ch = s.charAt(i);
+        System.out.println(ch);
+	}
+	}
+}
